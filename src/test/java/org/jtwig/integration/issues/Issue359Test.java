1a11e1a28bfd7b26ef503794785421f98ecfa64f
Casey Brooks <cjbrooks12@gmail.com>
2017-09-22 14:46:54 -0500

    Adds assertion that implemented interface methods have no issue

--- src/test/java/org/jtwig/integration/issues/Issue359Test.java
+++ src/test/java/org/jtwig/integration/issues/Issue359Test.java
@@ -9,10 +9,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Issue359Test {
 
-    public interface TestInterface {
+    public interface TestDefaultInterface {
         default String testDefaultInterfaceMethod() {
             return "default interface method";
         }
+    }
+
+    public interface TestNonDefaultInterface {
+        String testNonDefaultInterfaceMethod();
     }
 
     public static class TestParentClass {
@@ -21,19 +25,23 @@ public class Issue359Test {
         }
     }
 
-    public static class ClassUnderTest extends TestParentClass implements TestInterface {
-
+    public static class ClassUnderTest extends TestParentClass implements TestDefaultInterface, TestNonDefaultInterface {
+        @Override
+        public String testNonDefaultInterfaceMethod() {
+            return "non-default interface method";
+        }
     }
 
     @Test
     public void issue359() throws Exception {
         ClassUnderTest classUnderTest = new ClassUnderTest();
-        JtwigTemplate template = JtwigTemplate.inlineTemplate("[{{ value.testDefaultInterfaceMethod() }}] [{{ value.testParentClassMethod() }}]");
+        JtwigTemplate template = JtwigTemplate.inlineTemplate("[{{ value.testDefaultInterfaceMethod() }}] [{{ value.testNonDefaultInterfaceMethod() }}] [{{ value.testParentClassMethod() }}]");
 
         String result = template.render(JtwigModel.newModel().with("value", classUnderTest));
-        assertThat(result, is("[] [parent class method]"));
+        assertThat(result, is("[] [non-default interface method] [parent class method]"));
 
         assertThat(classUnderTest.testDefaultInterfaceMethod(), is("default interface method"));
+        assertThat(classUnderTest.testNonDefaultInterfaceMethod(), is("non-default interface method"));
         assertThat(classUnderTest.testParentClassMethod(), is("parent class method"));
     }
 }
